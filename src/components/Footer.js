219c41de99753d21857af97637e756c64b603071
Nick Marshall <marshalllele@uri.edu>
2024-01-24 17:13:54 -0500

    Adding starter files for portfolio site

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/Footer.css';
+
+function Footer() {
+  return (
+    <div>
+        <footer className='footer'>
+            <div className='foot-container'>
+                
+            </div>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
